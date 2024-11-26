b44a79094f14d98f64db7a70ef64ff4f312cea39
Abdullatif Khayat <abdullatif.khayat123@gmail.com>
2024-11-26 17:53:32 +0200

    setting up the store redux and redux toolkit and make the get request to show the data of cate and products

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,44 +1,33 @@
 import { Category } from "@components/eCommerce";
 import { Container, Row, Col } from "react-bootstrap";
-
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@store/hooks";
+import { actGetCategories } from "@store/categories/categoriesSlice";
 const Categories = () => {
+  const dispatch = useAppDispatch();
+  const { loading, error, records } = useAppSelector(
+    (state) => state.categories
+  );
+  useEffect(() => {
+    if (records.length === 0) dispatch(actGetCategories());
+  }, [dispatch, records]);
+
+  const categoriesList =
+    records.length > 0
+      ? records.map((records) => (
+          <Col
+            key={records.id}
+            xs={6}
+            md={3}
+            className="d-flex justify-content-center mb-5 mt-2"
+          >
+            <Category {...records} />
+          </Col>
+        ))
+      : "There are no records";
   return (
     <Container>
-      <Row>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-      </Row>
+      <Row>{categoriesList}</Row>
     </Container>
   );
 };
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,26 +1,41 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@store/hooks";
+import { useParams } from "react-router-dom";
+import {
+  actGetProductsByCatPrefix,
+  productsCleanUp,
+} from "@store/products/productsSlice";
 import { Container, Row, Col } from "react-bootstrap";
 import { Product } from "@components/eCommerce";
-
 const Products = () => {
+  const params = useParams();
+  const dispatch = useAppDispatch();
+  const { loading, error, records } = useAppSelector((state) => state.products);
+  useEffect(() => {
+    if (params.prefix && typeof params.prefix === "string")
+      dispatch(actGetProductsByCatPrefix(params.prefix));
+
+    return () => {
+      dispatch(productsCleanUp());
+    };
+  }, [dispatch, params]);
+
+  const productsList =
+    records.length > 0
+      ? records.map((records) => (
+          <Col
+            xs={6}
+            md={3}
+            key={records.id}
+            className="d-flex justify-content-center mb-5 mt-2"
+          >
+            <Product {...records} />
+          </Col>
+        ))
+      : "There are no products for this categories";
   return (
     <Container>
-      <Row>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Product />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Product />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Product />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Product />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Product />
-        </Col>
-      </Row>
+      <Row>{productsList}</Row>
     </Container>
   );
 };
